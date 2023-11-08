688fe4a32c52b4b3f9907097b60d0e88221834df
Dan Lund <dan.lundh@outlook.com>
2023-11-08 08:33:26 +0100

    Fix in FinCustomer Id Blazor

--- Data/Service.cs
+++ Data/Service.cs
@@ -8,6 +8,6 @@ public class Service
     public static List<Customer> customers {get; set;} = new List<Customer>();
     //public static List<CustomerReview> reviewlist {get; set;} = new List<CustomerReview>();
 
-    Customer mycust = new Customer(1,"Anders","Andersson", "19700304-1345","Storgatan 42, 43431 Kungsbacka","anders@hotmail.com","034-121121");
+    //Customer mycust = new Customer(1,"Anders","Andersson", "19700304-1345","Storgatan 42, 43431 Kungsbacka","anders@hotmail.com","034-121121");
 }
 
